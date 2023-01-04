a18781db447f306dbc669df251e51dbca1869d0f
Ramesh <ramesh.it11@gmail.com>
2023-01-04 17:18:40 +0530

    added hungry prompt

--- FirstFile.py
+++ FirstFile.py
@@ -1,1 +1,11 @@
-print("My first git file for demo")
+hungry=input("Are you hungry? ")
+if hungry=="yes":
+    print ("Eat Samosa")
+elif hungry=="Yes":
+    print("Eat Samosa")
+elif hungry=="Y":
+    print("Eat Samosa")
+elif hungry=="y":
+    print("Eat Samosa")
+else:
+    print("do your homework")
